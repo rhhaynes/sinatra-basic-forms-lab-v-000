1272e63f1dd1c43dbb7765132a85af7051423282
Randall Harris Haynes <harrishaynes@gmail.com>
2018-03-09 23:53:37 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,12 +4,15 @@ require_relative 'models/puppy.rb'
 class App < Sinatra::Base
   
   get '/' do
+    erb :index
   end
   
   post '/new' do
+    erb :create_puppy
   end
   
   get '/new' do
+    erb :display_puppy
   end
   
 end
